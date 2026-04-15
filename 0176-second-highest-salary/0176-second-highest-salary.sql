ce0244a139618d6763ffee92c0f27eb1e130a8d8
sitanshu4933 <62454843+sitanshu4933@users.noreply.github.com>
2026-04-15 10:55:02 +0530

    Time: 370 ms (54.85%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -10,19 +10,19 @@
 
 --Solution - 2 
 ----------------------------------
-SELECT (
-SELECT DISTINCT salary 
-FROM employee o
-WHERE 1 = (SELECT COUNT(DISTINCT SALARY)
-            FROM employee i
-            WHERE i.salary > o.salary))  AS SecondHighestSalary;
+-- SELECT (
+-- SELECT DISTINCT salary 
+-- FROM employee o
+-- WHERE 1 = (SELECT COUNT(DISTINCT SALARY)
+--             FROM employee i
+--             WHERE i.salary > o.salary))  AS SecondHighestSalary;
 
 -- Solution - 3
 -----------------------------------
--- SELECT 
--- (SELECT salary
--- FROM employee
--- ORDER BY salary DESC
--- OFFSET 1 ROW
--- FETCH NEXT 1 ROWS ONLY) AS SecondHighestSalary;
+SELECT 
+(SELECT DISTINCT salary
+FROM employee
+ORDER BY salary DESC
+OFFSET 1 ROW
+FETCH NEXT 1 ROWS ONLY) AS SecondHighestSalary;
 
